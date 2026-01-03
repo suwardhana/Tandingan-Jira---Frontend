4754b9ac5c67c7d1ef6ec7bc92a4d43fc4320856
Suwardhana <suwar_dhana@yahoo.co.id>
2026-01-03 22:14:37 +0700

    change to 3344

--- api.ts
+++ api.ts
@@ -1,7 +1,7 @@
 import { Task, User, Sprint } from "./types";
 
 const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:3344/api";
 
 const headers = {
   "Content-Type": "application/json",
